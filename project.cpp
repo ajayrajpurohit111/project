7316ffe8a1c9f7194d2d45d8d9d3cc727cb2255a
ajayrajpurohit111 <49920828+ajayrajpurohit111@users.noreply.github.com>
2021-07-29 00:50:37 +0530

    adding error

--- project.cpp
+++ project.cpp
@@ -9,7 +9,7 @@ int findlargest(int at)
 { 
 	int max = 0,i; 
 	for (i = 0; i < 3; i++) {  
-	                          if (p[i].AT <= at) { 
+	                s          if (p[i].AT <= at) { 
 	                                               if(p[i].BT==p[max].BT)
 				                                      {
 				                                     	if(p[i].id<p[max].id)
